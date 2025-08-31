47bca3ef196b91caa94ba7af808184a691957fb7
Atiq-5 <atiq.webdev@gmail.com>
2025-08-31 20:10:49 +0600

    with psudo random code

--- Untitled1.c
+++ Untitled1.c
@@ -1,24 +1,30 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<time.h>
-#define max 5000
+#define max 10000
 int undg[max][max];
+int psudoRandom(int x){
+  int z = (7*x+4)%9;
+  return z;
+
+}
 int main()
 {
     int sum=0,edge=0,degA=0,degB=0,n;
     double total_time;
     clock_t start,end;
-    start = clock();
-    printf("Enter the number vertices: ");
+    printf("\n Enter the number vertices: ");
     scanf("%d",&n);
-    srand(2);
+    int x = 3;
     for(int i=0; i<n; i++){
         for(int j=0; j<n; j++){
-            int val = rand()%2;
-            undg[i][j]=val;
-            undg[j][i]=val;
+            int val = psudoRandom(x);
+            x = val;
+            undg[i][j]=val%2;
+            undg[j][i]=val%2;
         }
     }
+    start = clock();
     for(int i=0; i<n; i++){
         for(int j=0; j<n; j++){
             if(undg[i][j]==1){
@@ -33,12 +39,13 @@ int main()
         }
     }
     sum = degA+degB;
-    printf("Number of Vertices: %d\n",sum);
-    printf("Number of Edge: %d\n",edge);
-    printf("The total number of edges is(using handshaking theorem):(sum of degree/2=(%d/2)=%d\n",sum,sum/2);
+    printf("\n Number of Vertices: %d\n",sum);
+    printf("\n Number of Edge: %d\n",edge);
+    printf("\n The total number of edges is(using handshaking theorem): \n");
+    printf(" Edge = (sum of degree/2) = (%d/2) = %d\n",sum,sum/2);
     end =clock();
     total_time = ((double)(end-start)/CLOCKS_PER_SEC)*1000;
-    printf("Time to execute: %lf ms",total_time);
+    printf("\n Time to execute: %lf ms\n",total_time);
     return 0;
 }
 
